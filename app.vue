0319682e0b4c57cba432a6d1bee54988c79cfbcc
Juan-pierre <juanpierre.engelbrecht@jumbo.com>
2024-05-07 16:28:08 +0200

    feat(capacitor): reproducing capacitor http error

--- app.vue
+++ app.vue
@@ -1,37 +1,50 @@
+<template>
+  <div>
+    <h3>Capacitor http error</h3>
+    <p v-if="responseData">Response Data:</p>
+    <ul v-if="responseData">
+      <li v-for="item in responseData" :key="item.id">{{ item.body }}</li>
+    </ul>
+
+  </div>
+</template>
+
 <script setup lang="ts">
-import {CapacitorHttp, type HttpResponse} from '@capacitor/core';
+import { ref } from 'vue';
 
+const responseData = ref(null);
 
-try {
-  const fetchResolve = await useFetch(
-      'https://jsonplaceholder.typicode.com/comments?postId=2'
-  );
-  console.log('useFetch', fetchResolve.data.value);
-} catch (e) {
-  console.error('useFetch', e);
-}
+const makeXmlHttpRequest = () => {
+  const _request = new XMLHttpRequest();
 
-try {
-  const $fetchResolve = await $fetch(
-      'https://jsonplaceholder.typicode.com/comments?postId=2'
-  );
-  console.log('$fetch', $fetchResolve);
-} catch (e) {
-  console.error('$fetch', e);
-}
-try {
-  const capacitorResponse: HttpResponse = await CapacitorHttp.get({
-    url: 'https://jsonplaceholder.typicode.com/comments?postId=2'
-  });
-  console.log('CapacitorHttp.get', capacitorResponse);
-} catch (e) {
-  console.error('CapacitorHttp.get', e);
-}
+  _request.onreadystatechange = function () {
+
+    console.log('Request readyState', _request.readyState)
+
+    // this request will fail when capacitorHttp is true
+    // CapacitorHttp.get is not checking the following condition
+    if (_request.readyState === XMLHttpRequest.DONE || _request.readyState === 4) {
+      if (_request.status === 200) {
+        try {
+          const parsed = JSON.parse(_request.responseText);
+          responseData.value = parsed;
+          console.log('XMLHttpRequest', parsed);
+        } catch (e) {
+          console.error('Error parsing response:', e);
+        }
+      } else {
+        console.error('Request failed with status:', _request.status);
+      }
+    }
+  };
+
+  _request.open('GET', 'https://jsonplaceholder.typicode.com/comments?postId=2', true);
+  _request.send();
+};
+
+makeXmlHttpRequest();
+
 
 </script>
 
-<template>
-  <div>
-    <p>Capacitor http error</p>
-  </div>
-</template>
+
